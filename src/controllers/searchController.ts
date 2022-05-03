8196992dcfe5c82e66229cd4c64cec2fdf50222e
Fabricio Gomes <54582091+fahfgomes@users.noreply.github.com>
2022-05-02 22:00:16 -0300

    final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -6,12 +6,16 @@ import { createMenuObject } from '../helpers/createMenuObjetc';
 
 export const search = (req: Request, res: Response) =>{
     let query: string = req.query.q as string;
-
+    if(!query){
+        res.redirect('/');
+        return;
+    }
     let list = Pet.getFromName(query);
 
-    res.render('pages/page'), {
+    res.render('pages/page', {
         menu: createMenuObject(''),
-        list
-    }
+        list,
+        query
+    });
 
 }
